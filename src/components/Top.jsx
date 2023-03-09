fc60c0eb66fc06baeb30462fbf9b3e643ff42e77
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-03-09 20:51:55 +0000

    Add layout

---
+++ src/components/Top.jsx
@@ -0,0 +1,9 @@
+
+export default function Top() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen w-screen">
+            <h1 className="text-4xl text-secondary">Hackathon</h1>
+            <h2>BugsByte</h2>
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        
+      },
+      colors: {
+        primary: '#000000',
+        secondary: '#00E600'
+      }
+    },
+  },
+  plugins: [],
+}
