c0595572008409386558c1e689adb8bfc6faad95
Mário Rodrigues <mjsrodrigues4@gmail.com>
2023-03-13 18:44:40 +0000

    Update layout for mobile

--- src/components/Podium.jsx
+++ src/components/Podium.jsx
@@ -3,86 +3,94 @@ import ampulheta from "public/ampulheta.json";
 
 export default function Podium() {
   return (
-    <div className="space-between font-terminal-uppercase flex h-screen w-screen w-screen flex-row items-center text-4xl text-white">
-      <div className="flex w-1/3 flex-col items-center">
-        <h1 className="text-secondary">Temas</h1>
-        <div className="mb-10 mt-10 flex flex-row items-center">
-          <h2 className="">Tema 1</h2>
-          <Lottie
-            animationData={ampulheta}
-            loop={true}
-            autoplay={true}
-            style={{ width: "50px", height: "50px" }}
-          />
-        </div>
-        <div className="mb-10 mt-10 flex flex-row items-center">
-          <h2 className="">Tema 2</h2>
-          <Lottie
-            animationData={ampulheta}
-            loop={true}
-            autoplay={true}
-            style={{ width: "50px", height: "50px" }}
-          />
-        </div>
-        <div className="mb-10 mt-10 flex flex-row items-center">
-          <h2 className="">Tema 3</h2>
-          <Lottie
-            animationData={ampulheta}
-            loop={true}
-            autoplay={true}
-            style={{ width: "50px", height: "50px" }}
-          />
-        </div>
-      </div>
-      <div className="flex w-1/3 flex-col items-center">
-        <h1 className="text-secondary">Prémios por Equipa</h1>
-        <div className="mb-10 mt-10 flex flex-col items-center">
-          <h2 className="text-white">Vencedores Gerais</h2>
-          <h2 className="text-white">1º Lugar - 600€</h2>
-          <h2 className="text-white">2º Lugar - 400€</h2>
-          <h2 className="text-white">3º Lugar - 200€</h2>
-        </div>
-        <div className="flex flex-col items-center ">
-          <h2 className="text-white">Vencedores Tema 1</h2>
-          <h2 className="text-white">1º Lugar - 300€</h2>
-        </div>
-        <div className="flex flex-col  items-center">
-          <h2 className="text-white">Vencedores Tema 2</h2>
-          <h2 className="text-white">1º Lugar - 300€</h2>
-        </div>
-        <div className="flex flex-col  items-center">
-          <h2 className="text-white">Vencedores Tema 3</h2>
-          <h2 className="text-white">1º Lugar - 300€</h2>
+    <div className="space-between font-terminal-uppercase lg:h-screen-lg xl:h-screen-xl flex h-screen w-screen flex-col items-center justify-center text-base text-white lg:flex-row lg:text-2xl xl:flex-row xl:text-4xl">
+      <div className="mt-10 flex flex flex-col items-center xl:mt-0 xl:w-1/3">
+        <h1 className="text-4xl text-secondary">Temas:</h1>
+        <div className="flex flex-row items-center xl:mb-10 xl:mt-10 xl:flex-col">
+          <div className="flex flex-row items-center xl:mb-10 xl:mt-10">
+            <h2 className="">Tema 1</h2>
+            <Lottie
+              animationData={ampulheta}
+              loop={true}
+              autoplay={true}
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
+          <div className="flex flex-row items-center xl:mb-10 xl:mt-10">
+            <h2 className="">Tema 2</h2>
+            <Lottie
+              animationData={ampulheta}
+              loop={true}
+              autoplay={true}
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
+          <div className="flex flex-row items-center xl:mb-10 xl:mt-10">
+            <h2 className="">Tema 3</h2>
+            <Lottie
+              animationData={ampulheta}
+              loop={true}
+              autoplay={true}
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
         </div>
       </div>
-      <div className="flex w-1/3 flex-col items-center">
-        <h1 className="text-secondary">Sponsors</h1>
-        <div className="mb-10 mt-10 flex flex-row items-center">
-          <h2 className="">Sponsor Exclusive</h2>
-          <Lottie
-            animationData={ampulheta}
-            loop={true}
-            autoplay={true}
-            style={{ width: "50px", height: "50px" }}
-          />
+      <div className="mt-10 flex flex-col items-center xl:mt-0 xl:w-1/3">
+        <h1 className="text-4xl text-secondary">Prémios por Equipa</h1>
+        <div className="flex flex-col items-center xl:mb-10">
+          <div className="flex flex-col items-center xl:mb-10 xl:mt-10 ">
+            <h2 className="text-white">Vencedores Gerais</h2>
+            <h2 className="text-white">1º Lugar - 600€</h2>
+            <h2 className="text-white">2º Lugar - 400€</h2>
+            <h2 className="text-white">3º Lugar - 200€</h2>
+          </div>
+          <div className="flex flex-row items-center xl:mb-10 xl:mt-10 xl:flex-col">
+            <div className="mt-4 flex flex-col items-center ">
+              <h2 className="text-white">Vencedores Tema 1</h2>
+              <h2 className="text-white">1º Lugar - 300€</h2>
+            </div>
+            <div className="mt-4 flex flex-col items-center ">
+              <h2 className="text-white">Vencedores Tema 2</h2>
+              <h2 className="text-white">1º Lugar - 300€</h2>
+            </div>
+            <div className="mt-4 flex flex-col items-center">
+              <h2 className="text-white">Vencedores Tema 3</h2>
+              <h2 className="text-white">1º Lugar - 300€</h2>
+            </div>
+          </div>
         </div>
-        <div className="mb-10 mt-10 flex flex-row items-center">
-          <h2 className="">Sponsor Gold</h2>
-          <Lottie
-            animationData={ampulheta}
-            loop={true}
-            autoplay={true}
-            style={{ width: "50px", height: "50px" }}
-          />
-        </div>
-        <div className="mb-10 mt-10 flex flex-row items-center">
-          <h2 className="">Sponsor Gold</h2>
-          <Lottie
-            animationData={ampulheta}
-            loop={true}
-            autoplay={true}
-            style={{ width: "50px", height: "50px" }}
-          />
+      </div>
+      <div className="mt-10 flex w-1/3 flex-col items-center xl:mt-0">
+        <h1 className="text-4xl text-secondary">Sponsors</h1>
+        <div className="flex flex-row items-center xl:flex-col">
+          <div className="flex flex-col items-center xl:mb-10 xl:mt-10 xl:flex-row">
+            <h2 className="">Sponsor Exclusive</h2>
+            <Lottie
+              animationData={ampulheta}
+              loop={true}
+              autoplay={true}
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
+          <div className="flex flex-col items-center xl:mb-10 xl:mt-10 xl:flex-row">
+            <h2 className="">Sponsor Gold</h2>
+            <Lottie
+              animationData={ampulheta}
+              loop={true}
+              autoplay={true}
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
+          <div className="flex flex-col items-center xl:mb-10 xl:mt-10 xl:flex-row">
+            <h2 className="">Sponsor Gold</h2>
+            <Lottie
+              animationData={ampulheta}
+              loop={true}
+              autoplay={true}
+              style={{ width: "50px", height: "50px" }}
+            />
+          </div>
         </div>
       </div>
     </div>
