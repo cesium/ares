c77b18e0701791e643f5a236709d452b98288974
Mário Rodrigues <mjsrodrigues4@gmail.com>
2024-02-19 13:53:18 +0000

    feat: setup supabase

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -13,16 +13,16 @@ cd ares
 ```
 
 Then all the commands you'll need to start developing are here:
-| Command                | Action                                            |
+| Command | Action |
 | :--------------------- | :------------------------------------------------ |
-| `npm install`          | Install dependencies                              |
-| `npm run dev`          | Start local dev server at `localhost:4321`        |
-| `npm run build`        | Build your production site to `./dist/`           |
-| `npm run preview`      | Preview your build locally, before deploying      |
-| `npm run astro ...`    | Run CLI commands like `astro add`, `astro check`  |
-| `npm run astro --help` | Get help using the Astro CLI                      |
-| `npm run format`       | Format code with [Prettier](https://prettier.io/) |
-| `npm run clean`        | Remove `node_modules` and build output            |
+| `npm install` | Install dependencies |
+| `npm run dev` | Start local dev server at `localhost:4321` |
+| `npm run build` | Build your production site to `./dist/` |
+| `npm run preview` | Preview your build locally, before deploying |
+| `npm run astro ...` | Run CLI commands like `astro add`, `astro check` |
+| `npm run astro --help` | Get help using the Astro CLI |
+| `npm run format` | Format code with [Prettier](https://prettier.io/) |
+| `npm run clean` | Remove `node_modules` and build output |
 
 ## 🔼 Opening a PR
 
@@ -38,10 +38,9 @@ git commit -m "Your commit message"
 git push
 ```
 
-Now all that is left to do is opening a Pull Request so your contribution can be published into production. If you don't know how to open a Pull Request, please refer to [this guide](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request). **_Don't forget to select the reviewers_**. 
+Now all that is left to do is opening a Pull Request so your contribution can be published into production. If you don't know how to open a Pull Request, please refer to [this guide](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request). **_Don't forget to select the reviewers_**.
 Once your PR is approved, your commit will be pushed to main and to production. Congratulations, you just contributed to the Ares repository.
 
 ### Note for those not a part of the CeSIUM team
 
 If you are not a member of our team on Github, then instead of creating a branch you need to fork this project. If you don't know how to do that, please [refer to this guide](https://docs.github.com/en/get-started/quickstart/fork-a-repo).
-
--- src/components/faqs.jsx
+++ src/components/faqs.jsx
@@ -1,21 +1,26 @@
-import { Disclosure } from '@headlessui/react'
-import { PlayIcon } from '@heroicons/react/24/solid'
+import { Disclosure } from "@headlessui/react";
+import { PlayIcon } from "@heroicons/react/24/solid";
 // import { getEntry } from 'astro:content';
-import faqs from "~/data/faqs.json"
+import faqs from "~/data/faqs.json";
 
 export default function Faqs() {
   return (
     <div>
       <div className="mx-auto max-w-7xl px-6 pb-24 sm:pb-32 lg:px-8 lg:pb-40">
         <div className="mx-auto max-w-4xl">
-          <h2 className="text-2xl font-bold leading-10 tracking-wide text-white">Frequently asked questions</h2>
+          <h2 className="text-2xl font-bold leading-10 tracking-wide text-white">
+            Frequently asked questions
+          </h2>
           <p className="mt-6 text-justify text-xl leading-7">
-            Have a different question and can’t find the answer you’re looking for?
-            Reach out to our support team by <a
+            Have a different question and can’t find the answer you’re looking
+            for? Reach out to our support team by{" "}
+            <a
               href="mailto:geral@bugsbyte.org"
               className="font-semibold text-indigo-400 hover:text-indigo-300"
-              >sending us an email </a
-            >and we’ll get back to you as soon as we can.
+            >
+              sending us an email{" "}
+            </a>
+            and we’ll get back to you as soon as we can.
           </p>
         </div>
         <div className="mx-auto max-w-4xl divide-y">
@@ -28,18 +33,27 @@ export default function Faqs() {
                       <Disclosure.Button className="flex w-full items-start justify-start text-left text-white">
                         <div>
                           {open ? (
-                            <PlayIcon fill="#66B22E" className="mt-2 mr-3 w-4 h-4 rotate-90" />
+                            <PlayIcon
+                              fill="#66B22E"
+                              className="mt-2 mr-3 w-4 h-4 rotate-90"
+                            />
                           ) : (
-                            <PlayIcon fill="#66B22E" className="mt-2 mr-3 w-4 h-4" />
+                            <PlayIcon
+                              fill="#66B22E"
+                              className="mt-2 mr-3 w-4 h-4"
+                            />
                           )}
                         </div>
-                        <span className="text-xl text-white font-semibold leading-7">{faq.question}</span>
-                        <span className="ml-6 flex h-7 items-center">
+                        <span className="text-xl text-white font-semibold leading-7">
+                          {faq.question}
                         </span>
+                        <span className="ml-6 flex h-7 items-center"></span>
                       </Disclosure.Button>
                     </dt>
                     <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                      <p className="font-sans text-base leading-7 text-white ml-7">{faq.answer}</p>
+                      <p className="font-sans text-base leading-7 text-white ml-7">
+                        {faq.answer}
+                      </p>
                     </Disclosure.Panel>
                   </>
                 )}
@@ -49,5 +63,5 @@ export default function Faqs() {
         </div>
       </div>
     </div>
-  )
+  );
 }
