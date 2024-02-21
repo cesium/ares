85820771d999a5d3657680c61e8344269e68a114
daniel_sp <danielsp356@gmail.com>
2024-02-21 10:21:03 +0000

    refactor: forms layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -27,5 +27,8 @@ module.exports = {
   corePlugins: {
     fontSize: false,
   },
-  plugins: [require("tailwindcss-fluid-type")],
+  plugins: [
+    require("tailwindcss-fluid-type"),
+    require('@tailwindcss/forms')
+  ],
 };
