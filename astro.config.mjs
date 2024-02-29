bce00d3cbd72c2368a60329337b3ffac8388038f
daniel_sp <danielsp356@gmail.com>
2024-02-29 10:42:42 +0000

    feat: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [tailwind(), icon(), react()],
   output: 'hybrid',
+  adapter: netlify()
 });
