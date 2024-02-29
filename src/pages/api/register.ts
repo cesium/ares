92d30dd988b28b69a94c8d88cff52639bfac7aa6
daniel_sp <danielsp356@gmail.com>
2024-02-29 10:36:44 +0000

    feat: add supabase schema locally

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -3,10 +3,12 @@ import type { APIRoute } from "astro";
 import { createClient } from '@supabase/supabase-js'
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 import type { DataItem } from "~/types";
+import { boolean } from 'astro/zod';
 
 const supabase = createClient(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_ANON_KEY)
 const client = new SESClient({ region: "eu-west-2" });
 const senderEmail = import.meta.env.SENDER_EMAIL;
+const discordInvite = import.meta.env.DISCORD_INVITE;
 
 const { randomUUID } = new ShortUniqueId({ length: 6 });
 
@@ -38,42 +40,41 @@ export const POST: APIRoute = async ({ request }) => {
     vegan: formData.get("vegan") === 'on'
   };
 
-
-  if (errors.length > 0) {
-    const { error } = await supabase
-      .from('participants')
-      .insert([
-        data,
-      ])
-      .select()
-
-    if (error) {
-      errors.push(error.message)
-
-      return new Response(
-        JSON.stringify({
-          message: {errors: errors},
-          status: 500,
-        }),
-        { status: 500 }
-      );
-    }
-  }
-  
-  const filePath = `cv/${data.email}/${files[0].name}`;
-  const file = files[0]
-  const { data: string, error } = await supabase.storage.from('files').upload(filePath, file);
-
-  if (error) {
-    errors.push(error.message);
-    const errorCode = parseInt(error.statusCode);
+  const insertion_msg = await supabase.from('participants').insert([data]).select();
+  if (insertion_msg.error) {
+    let msg = "There was an error connecting to the server. Try again later.";
+    errors.push(msg);
+    console.error(msg);
 
     return new Response(
       JSON.stringify({
-        message: {errors: errors},
-        status: errorCode,
+        message: { errors: errors },
+        status: 500,
       }),
-      { status: errorCode }
+      { status: 500 }
+    );
+  }
+
+  const filePath = `cv/${data.email}/${files[0].name}`;
+  const file = files[0]
+  const file_insertion_msg = await supabase.storage.from('files').upload(filePath, file);
+  if (file_insertion_msg.error) {
+    let msg = "There was an error connecting to the server file storage. Try again later.";
+    errors.push(msg);
+    console.error(msg);
+    
+    // delete previously created participant 
+    await supabase
+      .from('participants')
+      .delete()
+      .eq('email', data.email)
+
+    return new Response(
+      JSON.stringify({
+        message: { errors: errors },
+        status: 500,
+      }),
+      { status: 500 }
     );
   }
 
@@ -141,7 +142,7 @@ const sendConfirmationEmail = async(email: string, name: string, confirmation: s
             <div>
               <p>Your participation in the BugsByte Hackathon is confirmed! Your confirmation number is #${confirmation}</p>
               <p>Make sure to keep this email handy, it's your ticket to the event! Plus, stay tuned for more details as we'll be reaching out to you shortly with all the necessary information.</p>
-              <p>If you want to join our discord server, here's the link: http://google.com</p>
+              <p>If you want to join our discord server, here's the link: ${discordInvite}</p>
               <p>See you soon,</p>
               <p>Organization team 🪲</p>
             </div>`.toString()
@@ -159,3 +160,25 @@ const sendConfirmationEmail = async(email: string, name: string, confirmation: s
   }
 };
 
+const storeParticipant = async (data: DataItem): Promise<boolean> => {
+  try {
+    return true; // No error occurred
+  } catch (error) {
+    console.error("Error storing participant:", error);
+    return false; // Error occurred
+  }
+};
+
+const storeFile = async (files: any, email: string): Promise<boolean> => {
+  try {
+
+    if (message.error) {
+      return true
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error("Error storing the file:", error)
+    return false;
+  }
+};
