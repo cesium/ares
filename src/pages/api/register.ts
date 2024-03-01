089a675d2f37ef2402af6cce00c81dbf990731ac
daniel_sp <danielsp356@gmail.com>
2024-03-01 22:01:49 +0000

    fix: opt out of prerender

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -4,6 +4,8 @@ import { createClient } from "@supabase/supabase-js";
 import type { DataItem } from "~/types";
 import { SMTPClient } from "emailjs";
 
+export const prerender = false;
+
 const supabase = createClient(
   import.meta.env.SUPABASE_URL,
   import.meta.env.SUPABASE_ANON_KEY,
