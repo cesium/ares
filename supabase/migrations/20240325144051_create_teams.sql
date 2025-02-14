b8aaf9fce76993394268c54c8479f4e7c5688655
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2025-02-14 14:14:00 +0000

    feat: redone migrations and reenabled register form

--- supabase/migrations/20240325144051_create_teams.sql
+++ supabase/migrations/20240325144051_create_teams.sql
@@ -3,5 +3,8 @@ create table
     code varchar primary key,
     created_at timestamp with time zone not null default now(),
     name character varying null,
+    num_team_mem smallint not null default 1,
+    total_value_payment numeric null,
+    paid boolean not null default false,
     constraint teams_name_key unique (name)
   ) tablespace pg_default;
