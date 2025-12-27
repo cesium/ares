1679997edfc408b432dd3262e26208ef735517a2
João Lobo <joaodlobo@gmail.com>
2025-12-27 01:35:02 +0000

    lint bull

--- lib/ares_web/live/app/payment.ex
+++ lib/ares_web/live/app/payment.ex
@@ -155,7 +155,7 @@ defmodule AresWeb.AppLive.Payment do
       team.payment_status == :paid ->
         info(socket, "Your team has already completed the payment.")
 
-      is_payment_in_progress?(team) ->
+      payment_in_progress?(team) ->
         info(
           socket,
           "A payment is already in progress for your team. Please complete it or contact support."
@@ -342,7 +342,7 @@ defmodule AresWeb.AppLive.Payment do
      |> redirect(to: ~p"/app/profile")}
   end
 
-  defp is_payment_in_progress?(team) do
+  defp payment_in_progress?(team) do
     Billing.list_payments_by_team(team.id)
     |> Enum.any?(fn payment -> payment.status == :pending end)
   end
